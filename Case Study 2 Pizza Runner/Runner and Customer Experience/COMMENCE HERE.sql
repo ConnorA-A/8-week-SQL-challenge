db14b24e7488e85f269f18cd028381656b8a8dc6
Connor Arnold <con.arnold@outlook.com>
2026-06-26 18:31:34 +0100

    Removed a mock file I forgot to delete

--- Case Study 2 Pizza Runner/Runner and Customer Experience/COMMENCE HERE.sql
+++
@@ -1,8 +0,0 @@
--- What was the difference between the longest and shortest delivery time for all orders?
-
-SELECT
-    runner_orders.order_id,
-    MAX(runner_orders.duration::NUMERIC) - MIN(runner_orders.duration::NUMERIC)
-FROM runner_orders
-WHERE runner_orders.distance != 'null'
-GROUP BY runner_orders.order_id
